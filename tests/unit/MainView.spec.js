9faaad0f619161c569daccfe768d4549888745d8
mbeche <mb.johnsonbece@gmail.com>
2019-03-17 13:19:10 -0500

    Test 2 (Failing): MainView renders RecipeListEntry and NewRecipeForm

--- tests/unit/MainView.spec.js
+++ tests/unit/MainView.spec.js
@@ -1,5 +1,7 @@
 import { shallowMount } from "@vue/test-utils";
 import MainView from "@/views/MainView";
+import RecipeListEntry from "@/components/RecipeListEntry";
+import NewRecipeForm from "@/components/NewRecipeForm";
 
 describe("MainView", () => {
   it("renders the component", () => {
@@ -8,5 +10,16 @@ describe("MainView", () => {
 
     // assert
     expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  it("renders main child components", () => {
+    // arrange
+    const wrapper = shallowMount(MainView);
+    const recipeListEntry = wrapper.find(RecipeListEntry);
+    const newRecipe = wrapper.find(NewRecipeForm);
+
+    // assert
+    expect(recipeListEntry.exists()).toBe(true);
+    expect(newRecipe.exists()).toBe(true);
   });
 });
